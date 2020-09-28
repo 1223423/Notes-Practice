de4538d1940f5a487060635c67cef33e91ae3616
Ben <bencerne@gmail.com>
2020-09-28 11:44:55 +0200

    Update w2e5.py

--- python_intro/week2/w2e5.py
+++ python_intro/week2/w2e5.py
@@ -3,18 +3,21 @@
 n = int(input("n: "))
 
 def sieve(limit):
+    # Start with 2 in the list so we only iterate odd numbers
     nonprime = set()
     primes = [2]
 
+    # Loop through each odd number
     for i in range(3, limit+1, 2):
+        # If we already sieved i out, go to next loop iteration
         if i in nonprime:
             continue
 
+        # For each number sieve all multiples of i
         for z in range(i*i, limit+1, i):
             nonprime.add(z)
 
         primes.append(i)
-
     return primes
 
 print(sieve(n))
